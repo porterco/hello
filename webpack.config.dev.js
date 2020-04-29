1f84f707358b7b3bf1924e1b590b83ab62d2bc37
porterco <52006510+porterco@users.noreply.github.com>
2020-04-29 16:55:40 -0500

    Add files via upload

---
+++ webpack.config.dev.js
@@ -0,0 +1,30 @@
+var path = require ('path');
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+
+module.exports = {
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.join(__dirname, './src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
---
+++ webpack.config.prod.js
@@ -0,0 +1,34 @@
+var path = require ('path');
+var webpack = require ('webpack');
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+module.exports = {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.join(__dirname, './src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    }),
+    //Eliminate duplicate packages when generating bundle
+    new webpack.optimize.DedupePlugin(),
+    //minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style','css']}
+    ]
+  }
+}
